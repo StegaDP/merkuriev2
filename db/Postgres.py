5b21f78157d840c3fcf01cb8d2d08bcb2ad5101b
disabendera <109168072+disabendera@users.noreply.github.com>
2023-09-22 02:15:29 +0700

    Update Postgres.py

--- db/Postgres.py
+++ db/Postgres.py
@@ -1,23 +1,42 @@
 import os
 
-import psycopg2
+import sqlite3
 from dotenv import load_dotenv
 
 
-class Postgres:
+class SQLite:
     def __init__(self):
-        load_dotenv("../.env")
-        env = os.environ
-        self.DB_NAME = env.get("DB_NAME")
-        self.DB_HOST = env.get("DB_HOST")
-        self.DB_USER = env.get("DB_USER")
-        self.DB_PASSWORD = env.get("DB_PASSWORD")
-        self.DB_PORT = env.get('DB_PORT')
-
-        self.conn = psycopg2.connect(
-            f"host='{self.DB_HOST}' port='{self.DB_PORT}' dbname='{self.DB_NAME}' user='{self.DB_USER}' password='{self.DB_PASSWORD}'"
+        # load_dotenv("../.env")
+        # env = os.environ
+        # self.DB_NAME = env.get("DB_NAME")
+        # self.DB_HOST = env.get("DB_HOST")
+        # self.DB_USER = env.get("DB_USER")
+        # self.DB_PASSWORD = env.get("DB_PASSWORD")
+        # self.DB_PORT = env.get('DB_PORT')
+        self.conn = sqlite3.connect(
+            'database.db'
+            # f"host='{self.DB_HOST}' port='{self.DB_PORT}' dbname='{self.DB_NAME}' user='{self.DB_USER}' password='{self.DB_PASSWORD}'"
         )
         self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS users(\
+                id INT PRIMARY KEY,\
+                can_pay INT,\
+                can_create INT,\
+                can_approve INT,\
+                name TEXT);\
+                ")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS reports(\
+                created_by INT PRIMARY KEY,\
+                document_name TEXT,\
+                description TEXT,\
+                creation_date TEXT,\
+                approve_date TEXT,\
+                pay_date TEXT,\
+                approved_by INT,\
+                payed_by INT,\
+                message_id INT);\
+                ")
+        self.commit()
 
     def commit(self):
         self.conn.commit()
@@ -53,3 +72,5 @@ class Postgres:
     def query(self, sql, params=None):
         self.cur.execute(sql, params or ())
         return self.fetchall()
+
+
